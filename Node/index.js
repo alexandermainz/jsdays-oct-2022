569a0036211affcffb06337916cbcff63140c10f
Alexander Schmitt <admin@ash-soft.de>
2022-10-19 10:31:22 +0200

    WiP

--- Node/index.js
+++ Node/index.js
@@ -1,10 +1,19 @@
 import Module from './module.js';
 import { createServer } from 'node:http';
+import { faker } from '@faker-js/faker';
 
 console.log('Hallo Welt');
 
 const m = new Module('klaus');
 console.log(m);
+
+// Faker-js (https://fakerjs.dev/guide/usage.html)
+let names = new Set();
+for (let i=0; i < 100; i++) {
+  const randomName = faker.name.fullName(); // Rowan Nikolaus
+  names.add(randomName);
+}
+console.log(names);
 
 createServer((req, res) => res.end('foo')).listen(8080, () =>
   console.log('listening')
