d10e5143b899f5b9e069aff70b8bdab97d44402f
Alexander Schmitt <admin@ash-soft.de>
2022-10-19 15:00:20 +0200

    Refactor part 2

--- Node/my-express-app/index.ts
+++ Node/my-express-app/index.ts
@@ -1,33 +1,10 @@
 import express from 'express';
-import { Response, Request } from 'express';
-import { User } from './user';
+import usersRouter from './users';
 
 const app = express();
 
 app.use(express.json);
 
-let users: User[] = [
-    {
-        id: 1,
-        firstName: 'John',
-        lastName: 'Doe'
-    },
-    {
-        id: 2,
-        firstName: 'Jane',
-        lastName: 'Doe'
-    }
-];
-
-app.get('/users', (request: Request, response: Response): void => {
-    response.json(users);
-});
-
-app.post('/users', (request: Request, response: Response) => {
-    const id = Math.max(...users.map((user) => user.id)) + 1;
-    const newUser = { ...request.body, id };
-    users.push(newUser);
-    response.json(newUser);
-});
+app.use('/users', usersRouter);
 
 app.listen(8080, () => console.log('listening to http://localhost:8080'));
---
+++ Node/my-express-app/users/index.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { Response, Request } from 'express';
+import { User } from './user';
+
+const usersRouter = Router();
+
+let users: User[] = [
+    {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe'
+    },
+    {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'Doe'
+    }
+];
+
+usersRouter.get('/', (request: Request, response: Response): void => {
+    response.json(users);
+});
+
+usersRouter.post('/users', (request: Request, response: Response) => {
+    const id = Math.max(...users.map((user) => user.id)) + 1;
+    const newUser = { ...request.body, id };
+    users.push(newUser);
+    response.json(newUser);
+});
+
+export default usersRouter;
